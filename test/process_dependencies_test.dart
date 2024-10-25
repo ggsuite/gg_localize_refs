3e0933c73a5a851fe7ac9c112d2823e151f785b3
Göran Hegenberg <goeran@hegenberg-privat.de>
2024-10-25 09:27:16 +0200

    100% test coverage

--- test/process_dependencies_test.dart
+++ test/process_dependencies_test.dart
@@ -1,10 +1,158 @@
 import 'dart:io';
 
 import 'package:gg_to_local/src/process_dependencies.dart';
+import 'package:path/path.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final dWorkspaceSucceed = Directory(
+    join(
+      'test',
+      'sample_folder',
+      'workspace_pd_succeed',
+    ),
+  );
+
+  setUp(() async {
+    // create the tempDir
+    createDirs(
+      [
+        dWorkspaceSucceed,
+      ],
+    );
+  });
+
+  tearDown(() {});
+
   group('Process dependencies', () {
+    group('processProject()', () {
+      group('should throw', () {
+        test('when project root was not found', () async {
+          Directory dNoProjectRootError = Directory(
+            join(dWorkspaceSucceed.path, 'no_project_root_error'),
+          );
+
+          createDirs([dNoProjectRootError]);
+
+          List<String> messages = [];
+
+          await expectLater(
+            processProject(
+              dNoProjectRootError,
+              (
+                packageName,
+                pubspec,
+                pubspecContent,
+                yamlMap,
+                node,
+                projectDir,
+              ) async {},
+              messages.add,
+            ),
+            throwsA(
+              isA<Exception>().having(
+                (e) => e.toString(),
+                'message',
+                contains('No project root found'),
+              ),
+            ),
+          );
+        });
+
+        test('when node not found', () async {
+          Directory dNodeNotFound = Directory(
+            join(dWorkspaceSucceed.path, 'node_not_found'),
+          );
+
+          createDirs([dNodeNotFound]);
+
+          // Create a pubspec.yaml with invalid content in tempDir
+          File(join(dNodeNotFound.path, 'pubspec.yaml')).writeAsStringSync(
+            'name: test_package\nversion: 1.0.0\ndependencies:',
+          );
+
+          await expectLater(
+            processNode(
+              dNodeNotFound,
+              {},
+              {},
+              (
+                packageName,
+                pubspec,
+                pubspecContent,
+                yamlMap,
+                node,
+                projectDir,
+              ) async {},
+            ),
+            throwsA(
+              isA<Exception>()
+                  .having(
+                    (e) => e.toString(),
+                    'message',
+                    contains('node for the package'),
+                  )
+                  .having(
+                    (e) => e.toString(),
+                    'message',
+                    contains('not found'),
+                  ),
+            ),
+          );
+        });
+      });
+
+      test('succeeds', () async {
+        Directory dProject1 =
+            Directory(join(dWorkspaceSucceed.path, 'project1'));
+        Directory dProject2 =
+            Directory(join(dWorkspaceSucceed.path, 'project2'));
+
+        createDirs([dProject1, dProject2]);
+
+        File(join(dProject1.path, 'pubspec.yaml')).writeAsStringSync(
+          '''name: test1
+version: 1.0.0
+dependencies:
+  test2: ^1.0.0''',
+        );
+
+        File(join(dProject2.path, 'pubspec.yaml')).writeAsStringSync(
+          '''name: test2
+version: 1.0.0''',
+        );
+
+        List<String> messages = [];
+
+        await processProject(
+          dProject1,
+          (
+            packageName,
+            pubspec,
+            pubspecContent,
+            yamlMap,
+            node,
+            projectDir,
+          ) async {
+            expect(packageName, 'test1');
+            expect(pubspec.path, endsWith('pubspec.yaml'));
+            expect(pubspecContent, '''name: test1
+version: 1.0.0
+dependencies:
+  test2: ^1.0.0''');
+            expect(yamlMap, {
+              'name': 'test1',
+              'version': '1.0.0',
+              'dependencies': {'test2': '^1.0.0'},
+            });
+            expect(node.name, 'test1');
+            expect(projectDir.path, endsWith('project1'));
+          },
+          messages.add,
+        );
+      });
+    });
+
     group('Helper methods', () {
       group('correctDir()', () {
         test('succeeds', () {
@@ -37,4 +185,22 @@ void main() {
       });
     });
   });
+}
+
+void createDirs(List<Directory> dirs) {
+  for (final dir in dirs) {
+    if (!dir.existsSync()) {
+      dir.createSync(recursive: true);
+    }
+    expect(dir.existsSync(), isTrue);
+  }
+}
+
+void deleteDirs(List<Directory> dirs) {
+  for (final dir in dirs) {
+    if (dir.existsSync()) {
+      dir.deleteSync(recursive: true);
+    }
+    expect(dir.existsSync(), isFalse);
+  }
 }
